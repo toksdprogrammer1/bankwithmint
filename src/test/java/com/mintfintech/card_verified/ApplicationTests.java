fd4dec6499bdf02d09443456f52b8c901c3da503
Tokunbo.Ojo <tokunbo.ojo@interswitchgroup.com>
2020-06-26 01:18:40 +0100

    UNIT TEST

--- src/test/java/com/mintfintech/card_verified/ApplicationTests.java
+++ src/test/java/com/mintfintech/card_verified/ApplicationTests.java
@@ -2,11 +2,20 @@ package com.mintfintech.card_verified;
 
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mintfintech.card_verified.exception.BadGatewayException;
+import com.mintfintech.card_verified.exception.BadRequestException;
+import com.mintfintech.card_verified.service.CardService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.Assert.*;
 
 
 @RunWith(SpringRunner.class)
@@ -14,7 +23,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(locations = {"classpath:application.properties"})
 public class ApplicationTests {
 
+	@Autowired
+	private CardService cardService;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void test_invalidCard() {
+
+		try {
+			cardService.verify("xxxxxxx");
+			fail();
+		} catch (BadGatewayException | BadRequestException | JsonProcessingException ex) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void test_validCard() throws BadGatewayException, BadRequestException, JsonProcessingException {
+
+		assertNotNull(cardService.verify("45717360"));
 	}
 }
